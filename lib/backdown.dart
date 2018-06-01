5570872aca15849b9b2dafa67ed1652c5b0461f1
Tom Newton <tom.newton@gmail.com>
2018-06-01 15:51:53 -0500

    flutter analyze hangs

--- lib/backdown.dart
+++ lib/backdown.dart
@@ -63,7 +63,7 @@ class Backdown {
           print(downloadId);
           print(filePath);
 
-          Directory p = await getApplicationDocumentsDirectory();
+          /*Directory p = await getApplicationDocumentsDirectory();
           if (filePath.startsWith("file://")) {
             filePath = filePath.substring(7);
           }
@@ -77,7 +77,7 @@ class Backdown {
           File f = new File(newName);
           f.createSync(recursive: true);
           complete.renameSync(newName);
-          print("Moved to: $newName");
+          print("Moved to: $newName");*/
         }
         break;
       case PROGRESS_EVENT:
